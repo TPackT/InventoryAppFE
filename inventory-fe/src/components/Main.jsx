5fe7d435c80051d454d2448e32de450e2b3802c9
nguq03 <nguq03@vse.cz>
2025-07-27 05:08:02 +0200

    react setup

---
+++ inventory-fe/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import TopBar from "./TopBar"
+import ProductList from "./ProductList"
+
+export default function Main() {
+    return(
+        <main>
+            <TopBar />
+            <ProductList />
+        </main>
+    )
+}
---
+++ inventory-fe/src/components/ProductList.jsx
@@ -0,0 +1,38 @@
+export default function ProductList() {
+    return (
+        <section>
+            <header></header>
+            <ul className="product-list">
+                <li>
+                    <span>ID</span>
+                    <span>Name</span>
+                    <span>Desc</span>
+                    <span>Size</span>
+                    <span>Price</span>
+                    <span>Quantity</span>
+                    <span>Action</span>
+                </li>
+                <li>
+                    <span>1</span>
+                    <span>Coca-Cola</span>
+                    <span>Whatever</span>
+                    <span>500ml</span>
+                    <span>30</span>
+                    <span>2</span>
+                    <button>Detail</button>
+                </li>
+                <li>
+                    <span>2</span>
+                    <span>Fanta</span>
+                    <span>Whatever</span>
+                    <span>500ml</span>
+                    <span>30</span>
+                    <span>2</span>
+                    <button>Detail</button>
+                </li>
+                
+            </ul>
+        </section>
+        
+    )
+}
